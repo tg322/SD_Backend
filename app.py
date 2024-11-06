318bf3f1b821b4777aefc3954018c72a4e19c1e7
Tom Grennan <tom.shaun.grennan@gmail.com>
2024-11-06 11:50:00 +0000

    Updated Req.txt to include the correct jwt package Updated app.py to initialise app outside main function

--- app.py
+++ app.py
@@ -3,20 +3,19 @@ from flask import Blueprint, Flask
 from flask_cors import CORS
 from db_models import db  # Import only db, not all models globally if not needed
 
-
 # Define the Blueprint
 auth_bp = Blueprint('auth', __name__)
 
 # Import route handlers
 from user_access_endpoints import login_user, sign_up_user
 
-# Attach the route handler to the route
+# Attach the route handlers to the blueprint
 auth_bp.route('/login', methods=['POST'])(login_user)
 auth_bp.route('/sign_up', methods=['POST'])(sign_up_user)
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    CORS(app, resources={r"/*": {"origins": "*"}})
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/software_agile'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -25,6 +24,8 @@ def create_app():
 
     return app
 
+# Instantiate the app at the module level
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
