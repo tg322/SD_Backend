ad7111c40cdd130bd10bacb171dd0a5c51cac057
Tom Grennan <tom.shaun.grennan@gmail.com>
2024-11-20 19:21:48 +0000

    Added blueprint for tickets to app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def create_app():
 
     db.init_app(app)  # Initialize db with the app
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
-    
+    app.register_blueprint(tickets_bp, url_prefix='/api/tickets')  # url_prefix
 
     return app
 
